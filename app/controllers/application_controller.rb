21e6d4ecc9644b172e3dcb5c5b23dfe9f6186949
gefangshuai <gefangshuai@163.com>
2013-11-06 23:01:56 +0800

    add cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # 定义为似有的方法，表示只有控制器中才能调用
+  private
+  # 定义获得当前购物车通用函数
+  def current_cart
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart  
+  end
 end
